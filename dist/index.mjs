fc30bc4ea0f131ee2abe1aa7566deee78fe9a65c
Alan Layng <alan@readersludlow.com>
2020-01-15 16:43:46 +0000

    initial upload

---
+++ dist/index.mjs
@@ -0,0 +1,67 @@
+function promiseMap (PromiseLibrary = Promise) {
+  if (typeof PromiseLibrary !== 'function') return
+  if (!PromiseLibrary.prototype.map) {
+    PromiseLibrary.prototype.map = map;
+  }
+  if (!PromiseLibrary.map) {
+    PromiseLibrary.map = function map (iterable, ...args) {
+      return PromiseLibrary.resolve(iterable).map(...args)
+    };
+  }
+}
+function map (mapper, { concurrency = Infinity } = {}) {
+  const P = this.constructor;
+  return this.then(
+    iterable =>
+      new P((resolve, reject) => {
+        const iterator = getIterator(iterable);
+        let active = 0;
+        let count = 0;
+        const result = [];
+        let done;
+        fetchOne();
+        function fetchOne () {
+          if (done) return
+          const item = iterator.next();
+          if (thenable(item)) item.then(startOne, reject);
+          else startOne(item);
+        }
+        function startOne (item) {
+          if (item.done) {
+            done = true;
+            if (!active) resolve(result);
+            return
+          }
+          active++;
+          const idx = count++;
+          P.resolve(item.value)
+            .then(mapOne.bind(null, idx))
+            .then(storeOne.bind(null, idx), reject);
+          if (active < concurrency) fetchOne();
+        }
+        function mapOne (idx, val) {
+          return mapper(val, idx)
+        }
+        function storeOne (idx, val) {
+          result[idx] = val;
+          active--;
+          fetchOne();
+          if (done && !active) resolve(result);
+        }
+      })
+  )
+  function thenable (p) {
+    return p instanceof P || typeof p.then === 'function'
+  }
+}
+function getIterator (iterable) {
+  if (Symbol.asyncIterator in iterable) {
+    return iterable[Symbol.asyncIterator]()
+  }
+  if (Symbol.iterator in iterable) {
+    return iterable[Symbol.iterator]()
+  }
+  throw new TypeError('It is not an iterable')
+}
+
+export default promiseMap;
---
+++ test/test.js
@@ -0,0 +1,84 @@
+import test from 'ava'
+import promiseMap from '../src'
+
+promiseMap()
+
+test('installed', t => {
+  t.is(typeof Promise.map, 'function')
+  t.is(typeof Promise.prototype.map, 'function')
+})
+
+test('is idempotent', t => {
+  t.notThrows(promiseMap)
+})
+
+test('non promise lib', t => {
+  t.notThrows(() => promiseMap({}))
+})
+
+test('simple instance iterator', async t => {
+  const p = Promise.resolve([1, 2, 3]).map(x => 10 * x)
+  t.true(p instanceof Promise)
+  const result = await p
+  t.deepEqual(result, [10, 20, 30])
+})
+
+test('simple class iterator', async t => {
+  const p = Promise.map([1, 2, 3], x => 10 * x)
+  t.true(p instanceof Promise)
+  const result = await p
+  t.deepEqual(result, [10, 20, 30])
+})
+
+test('async instance iterator', async t => {
+  async function * iter () {
+    await delay(10)
+    yield 1
+    await delay(10)
+    yield 2
+    await delay(10)
+    yield 3
+  }
+
+  const p = Promise.resolve(iter()).map(x => x * 10)
+  t.true(p instanceof Promise)
+  const result = await p
+  t.deepEqual(result, [10, 20, 30])
+})
+
+test('async map function', async t => {
+  const p = Promise.map([3, 1, 2], async x => {
+    await delay(10 * x)
+    return 10 * x
+  })
+  const result = await p
+  t.deepEqual(result, [30, 10, 20])
+})
+
+test('empty iterable', async t => {
+  const p = Promise.map([], x => x)
+  t.deepEqual(await p, [])
+})
+
+test('concurrency limited', async t => {
+  let count = 0
+  async function fn (x) {
+    count++
+    t.true(count <= 3)
+    await delay(30)
+    count--
+    return x * 10
+  }
+
+  const result = await Promise.map([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], fn, {
+    concurrency: 3
+  })
+
+  t.deepEqual(result, [0, 10, 20, 30, 40, 50, 60, 70, 80, 90])
+})
+
+test('non-iterable', async t => {
+  await t.throwsAsync(() => Promise.map({}, x => x))
+})
+
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
